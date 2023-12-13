2b59ac6cf1b74820a77b00848278df3f559f4325
Ashley Goldstein <agoldstein@nvidia.com>
2023-12-13 11:17:13 -0500

    add jen change

--- exts/ash.winter.lights/ash/winter/lights/extension.py
+++ exts/ash.winter.lights/ash/winter/lights/extension.py
@@ -1,7 +1,11 @@
+import omni.usd
 import omni.ext
 import omni.ui as ui
-from .window import WinterLightsWindow
+from omni.ui import color as cl
+from pxr import Sdf, UsdGeom
+import random
 
+WINDOW_STYLE = {"Window": {"background_color": cl.white}}
 
 # Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
 def some_public_function(x: int):
@@ -17,10 +21,53 @@ class AshWinterLightsExtension(omni.ext.IExt):
     # this extension is located on filesystem.
     def on_startup(self, ext_id):
         print("[ash.winter.lights] ash winter lights startup")
+        self._points = None
+        self.instancer = None
+        self._window = ui.Window("Holiday Lights", width=300, height=300)
+        frame_style ={"ScrollingFrame": {"background_color": cl("#097eff90"), "border_color": cl.white}}
+        with self._window.frame:
+            with ui.ScrollingFrame(style=frame_style):
+                with ui.VStack(spacing=0):
+                    button_style = {"Button":{"background_color": cl("#097eff"),
+                                                    "border_color": cl.white,
+                                                    "border_width": 2.0,
+                                                    "padding": 10,
+                                                    "margin_height": 40,
+                                                    "border_radius": 10,
+                                                    "margin_width":15},
+                                                    "Button.Label":{"color": cl.white},
+                                                    "Button:hovered":{"background_color": cl("#E5F1FB")}}
+                    ui.Button("DECORATE", clicked_fn=self.get_points, style=button_style)
+                    # ui.Button("Place Lights", clicked_fn=self.place_lights)
+    
+    def get_points(self):
+        ctx = omni.usd.get_context()
+        stage = ctx.get_stage()
+        selection = ctx.get_selection()
+        if selection and len(selection.get_selected_prim_paths()) > 0:
+            prim_path = selection.get_selected_prim_paths()[0]
+            prim = stage.GetPrimAtPath(prim_path)
+            self._points = prim.GetAttribute("points").Get()
+            self.create_instancer(prim_path)
 
-        self._window = WinterLightsWindow("Winter Lights", width=300, height=300)
+    def create_instancer(self, prim_path):
+        stage = omni.usd.get_context().get_stage()
+        instancer_path = Sdf.Path(omni.usd.get_stage_next_free_path(stage, prim_path + "/LightInstancer", False))
+        self.instancer = UsdGeom.PointInstancer.Define(stage, instancer_path)
+        self.instancer.CreatePrototypesRel().SetTargets(["/World/Lights/holiday_light_red", "/World/Lights/holiday_light_green", "/World/Lights/holiday_light_blue"])
+        self.place_lights()
+
+    def place_lights(self):
+        if len(self._points) > 0:
+            new_points = [self._points[i] for i in range(0, len(self._points)) if i % 21 == 0]
+            print(len(self._points))
+            print(len(new_points))
+            ids = []
+            for i in new_points:
+                ids.append(random.randint(0,1000)%3)
+            self.instancer.CreatePositionsAttr().Set(new_points)
+            self.instancer.CreateProtoIndicesAttr().Set(ids)
 
     def on_shutdown(self):
-        print("[ash.winter.lights] ash winter lights shutdown")
         self._window.destroy()
         self._window = None
--- exts/ash.winter.lights/ash/winter/lights/window.py
+++ exts/ash.winter.lights/ash/winter/lights/window.py
@@ -1,71 +1,72 @@
 import omni.usd
 import omni.ui as ui
+import omni.ext
 from omni.ui import color as cl
-from .mesh_generator import MeshGenerator
-import math
+from pxr import Sdf, UsdGeom
+import random
 
 
 class WinterLightsWindow(ui.Window):
 
-    def __init__(self, title: str, **kwargs) -> None:
-        super().__init__(title, **kwargs)
-        self.generator: MeshGenerator = None
-
-        self.frame.set_build_fn(self._build_fn)
+    def __init__(self, title, width, height):
+        super().__init__(title, width=width, height=height)
+        self._build_fn = self._build
+        #self.frame.set_build_fn(self._build_fn)
         
-    def _build_window(self):
+
         with self.frame:
-            with ui.VStack():
-                curve_label = None
-                points_label = None
-
-
-            def get_selection():
-                #Get curve selected from stage
-                #if self.generator is None:
-                self.generator = MeshGenerator()
-                if self.generator.get_selection():
-                    curve_label.text = self.generator.selected_prim_path
-                else:
-                    curve_label.text = "No Selection"
-
-                #Get points along curve selected
-                if self.generator is not None:
-                    if self.generator.get_points_along_curve():
-                        points_label.text = "Points Stored"
-                    else:
-                        points_label.text = "Could not store points"
-
-            def generate_lights():
-                if self.generator is not None:
-                    self.generator.generate_lights()
-
-
-            with ui.VStack():
-                with ui.HStack():
-                    button_style = {"Button":{"background_color": cl.cyan,
-                                            "border_color": cl.white,
-                                            "border_width": 2.0,
-                                            "padding": 10,
-                                            "margin_height": 40,
-                                            "border_radius": 10,
-                                            "margin_width":15},
-                                            "Button.Label":{"color": cl.black},
-                                            "Button:hovered":{"background_color": cl("#E5F1FB")}}
-                    ui.Button("Grab Curve", style=button_style, clicked_fn=get_selection)
-                    curve_label = ui.Label("No Selection")
-                    with ui.ZStack():
-                        points_label = ui.Label("No Points Stored")
-
-                with ui.HStack():
-                    ui.Button("Generate Winter Lights", style=button_style, clicked_fn=generate_lights)
+            with ui.ScrollingFrame():
+                with ui.VStack(height=10):
+                    self._points = None
+                    self.instancer = None
                     
-
-    def _build_fn(self):
-        with ui.ScrollingFrame():
-            with ui.VStack(height=10):
-                self._build_window()
+                def get_points():
+                    ctx = omni.usd.get_context()
+                    stage = ctx.get_stage()
+                    selection = ctx.get_selection()
+                    if selection and len(selection.get_selected_prim_paths()) > 0:
+                        prim_path = selection.get_selected_prim_paths()[0]
+                        prim = stage.GetPrimAtPath(prim_path)
+                        self._points = prim.GetAttribute("points").Get()
+                        self.create_instancer(prim_path)
+            
+                def create_instancer(self, prim_path):
+                    stage = omni.usd.get_context().get_stage()
+                    instancer_path = Sdf.Path(omni.usd.get_stage_next_free_path(stage, prim_path + "/LightInstancer", False))
+                    self.instancer = UsdGeom.PointInstancer.Define(stage, instancer_path)
+                    self.instancer.CreatePrototypesRel().SetTargets(["/World/Lights/holiday_light_red", "/World/Lights/holiday_light_green", "/World/Lights/holiday_light_blue"])
+                    self.place_lights()
                 
+                def place_lights(self):
+                    if len(self._points) > 0:
+                        new_points = [self._points[i] for i in range(0, len(self._points)) if i % 21 == 0]
+                        print(len(self._points))
+                        print(len(new_points))
+                        ids = []
+                        for i in new_points:
+                            ids.append(random.randint(0,1000)%3)
+                        self.instancer.CreatePositionsAttr().Set(new_points)
+                        self.instancer.CreateProtoIndicesAttr().Set(ids)
+
+
+                with ui.VStack():
+                    with ui.HStack():
+                        button_style = {"Button":{"background_color": cl.cyan,
+                                                "border_color": cl.white,
+                                                "border_width": 2.0,
+                                                "padding": 10,
+                                                "margin_height": 40,
+                                                "border_radius": 10,
+                                                "margin_width":15},
+                                                "Button.Label":{"color": cl.black},
+                                                "Button:hovered":{"background_color": cl("#E5F1FB")}}
+                        ui.Button("Grab Points", style=button_style, clicked_fn=get_points)
+
+                    with ui.HStack():
+                        pass
+                        #ui.Button("Generate Winter Lights", style=button_style, clicked_fn=place_lights)
+                        
+                    
 
     def destroy(self):
         super().destroy()
